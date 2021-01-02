04b7701e679ca586b1984d312ca3af1619da4298
Android-Penguin <mail@shauncb.xyz>
2021-01-02 17:33:44 +1300

    Created Files
    - Created index HTML file, styles CSS file and form classes JSON array.
    
    - Added planning images, placeholder expand button and website icon.
    
    - Made basic layout and styling of dropdown box.

---
+++ form_classes.js
@@ -0,0 +1,49 @@
+var form_classes = [
+  {"teacher":"Miss Coombridge", "code":"13BC?", "location":"????",
+    "students":[
+      "student1",
+      "student2",
+      "student3",
+      "student4",
+    ]},
+
+  {"teacher":"Mr Thomson", "code":"13CTH", "location":"K101",
+    "students":[
+      "student1",
+      "student2",
+      "student3",
+      "student4",
+    ]},
+
+  {"teacher":"Mr Ward", "code":"13DWD", "location":"A205",
+    "students":[
+      "student1",
+      "student2",
+      "student3",
+      "student4",
+    ]},
+
+  {"teacher":"Mrs Price", "code":"13LPR", "location":"S207",
+    "students":[
+      "student1",
+      "student2",
+      "student3",
+      "student4",
+    ]},
+
+  {"teacher":"Mr Harris", "code":"13MTH", "location":"K101",
+    "students":[
+      "student1",
+      "student2",
+      "student3",
+      "student4",
+    ]},
+
+  {"teacher":"Mr Cleary", "code":"13SCL", "location":"A201",
+    "students":[
+      "student1",
+      "student2",
+      "student3",
+      "student4",
+    ]},
+]
